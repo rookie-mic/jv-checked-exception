abc1a935461a430593b17a8df4d3cd0df7697f85
Brynkiewicz <michal.brynkiewicz@envistaco.com>
2026-02-20 12:23:42 +0100

    exceptionTask - checkstyle fixes2

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -6,9 +6,8 @@ public class PasswordValidator {
         final int MinPassLength = 10;
 
         if (password != repeatPassword
-                && (password.length() <= MinPassLength || repeatPassword.length() <= MinPassLength))
-        {
-            throw new PasswordValidationException("Wrong passwords");
+                && (password.length() <= MinPassLength || repeatPassword.length() <= MinPassLength)) {
+                    throw new PasswordValidationException("Wrong passwords");
         };
     }
 }
