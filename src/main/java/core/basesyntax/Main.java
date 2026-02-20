54ffaef9a10895af0beb4105c9e31073ec2c1cc5
Brynkiewicz <michal.brynkiewicz@envistaco.com>
2026-02-20 12:00:19 +0100

    exceptionTask - frist solution

---
+++ src/main/java/core/basesyntax/Main.java
@@ -0,0 +1,9 @@
+package core.basesyntax;
+
+public class Main {
+    public static void main(String[] args) throws PasswordValidationException{
+        User user = new User("fff@ddd.com","11", "10");
+        UserService userService = new UserService();
+        userService.registerUser(user);
+    }
+}
--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,12 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) {
-        //write your code here
+    public void validate(String password, String repeatPassword) throws PasswordValidationException {
+        final int MIN_PASS = 10;
+
+        if (password != repeatPassword
+                && (password.length() <= MIN_PASS || repeatPassword.length() <= MIN_PASS)) {
+            throw new PasswordValidationException("Wrong passwords");
+        };
     }
 }
